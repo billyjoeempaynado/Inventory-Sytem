73f51a4465107ac3d9a28763c39e0aad069b5b90
Bijanggg <44258483+billyjoeempaynado@users.noreply.github.com>
2024-09-06 15:00:41 +0800

    Update

--- server.js
+++ server.js
@@ -1,27 +1,31 @@
 const express = require("express");
 const path = require("path");  // Import path module
+const { Pool } = require('pg');
 
-const invetoryRoutes = require("./src/product/routes")
+const inventoryRoutes = require("./src/product/routes")
 
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
-app.use(express.json());
+app.use(express.static(path.join(__dirname, "src")));
 
-app.get("/", (req, res) => {
-    res.send("index");
-});
+
 
 app.get("/src/register", (req, res) => {
     res.sendFile(path.join(__dirname, 'src', 'register.html'));
-});
+  });
 
 app.get("/src/login", (req, res) => {
     res.sendFile(path.join(__dirname, 'src', 'login.html'));
 });
 
-app.use("/api/v1/Inventory_System", invetoryRoutes);
+
+app.get("/src/index", (req, res) => {
+    res.sendFile(path.join(__dirname, 'src', 'index.html'));
+});
+
+app.use("/api/v1/Inventory_System", inventoryRoutes);
 
 app.listen(port, () => console.log(`app listening on port ${port}`));
 

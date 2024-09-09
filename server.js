c60826a5bdbc13e272fd80a97b92325d44dd1d25
Bijanggg <44258483+billyjoeempaynado@users.noreply.github.com>
2024-09-09 15:09:14 +0800

    Update 9-9

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");  // Import path module
-const { Pool } = require('pg');
+const { Pool } = require("./dbConfig");
+
 
 const inventoryRoutes = require("./src/product/routes")
 

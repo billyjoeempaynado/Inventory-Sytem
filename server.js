628c72ddcf2d999d24f0a25d2e5c490274368232
Bijanggg <44258483+billyjoeempaynado@users.noreply.github.com>
2024-09-12 11:13:43 +0800

    update logout, authenticate url

--- server.js
+++ server.js
@@ -40,16 +40,26 @@ app.get("/", (req, res) => {
     res.render("index");
 });
 
-app.get("/users/register", (req, res) => {
+app.get("/users/register", checkAuthenticated, (req, res) => {
     res.render("register");
 });
 
-app.get("/users/login", (req, res) => {
+app.get("/users/login", checkAuthenticated, (req, res) => {
     res.render("login");
 });
 
-app.get("/users/dashboard", (req, res) => {
-    res.render("dashboard", {user:"Billy"});
+app.get("/users/dashboard", checkNotAuthenticated, (req, res) => {
+    res.render("dashboard", {user: req.user.name});
+});
+
+app.get("/users/logout", (req, res, next) => {
+    req.logout(function(err) {
+        if (err) {
+            return next(err);
+        }
+        req.flash("success_msg", "You have logged out");
+        res.redirect("/users/login");
+    });
 });
 
 
@@ -124,6 +134,21 @@ app.post("/users/login", passport.authenticate("local", {
     })
 );
 
+
+function checkAuthenticated(req, res, next){
+    if (req.isAuthenticated()){
+        return res.redirect("/users/dashboard");
+    }
+    next();
+}
+
+function checkNotAuthenticated(req, res, next){
+    if (req.isAuthenticated()) {
+        return next();
+    }
+
+    res.redirect("/users/login");
+}
 
 app.use("/api/v1/Inventory_System", inventoryRoutes);
 app.use("/api/v1/Users", userRouter);

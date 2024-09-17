099f690458e6a0b2f8157b2ebfef188833cbb594
Bijanggg <44258483+billyjoeempaynado@users.noreply.github.com>
2024-09-17 15:48:15 +0800

    update products queries

--- src/product/productQueries.js
+++ src/product/productQueries.js
@@ -1,9 +1,9 @@
 
 const getProducts = "SELECT * FROM products";
 const getProductById = "SELECT * FROM products WHERE id = $1";
-const addProduct = "INSERT INTO items (product_name, price) VALUES ($1, $2)";
-const updateProduct = "UPDATE product SET product_name = $1 WHERE id = $2";
-const deleteProduct = "DELETE FROM product WHERE id = $1";
+const addProduct = "INSERT INTO products (product_name, price) VALUES ($1, $2)";
+const updateProduct = "UPDATE products SET product_name = $1 WHERE id = $2";
+const deleteProduct = "DELETE FROM products WHERE id = $1";
 
 // const checkEmailExist = "SELECT s FROM TABLE_NAME s WHERE s.email = $1";
 // const addStudent = "INSERT INTO TABLE_NAME (name, email, age, dob) VALUES ($1, $2, $3, $4)";
